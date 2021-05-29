da53375ac4602e5ba565f68fa3f45cd61885bd2d
Drudgerist <v.urrila@gmail.com>
2021-05-30 00:17:40 +0200

    Removed FPS display, will create my own graph to display this later anyway.

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -69,7 +69,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
 	const cocos2d::Size dynamicSize = glview->getFrameSize();
 
     // turn on display FPS
-    director->setDisplayStats(true);
+//    director->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
